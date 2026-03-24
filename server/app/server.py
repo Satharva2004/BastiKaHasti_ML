02ebab2ad1ed593c2729782ac705b3f690e8fb19
Satharva2004 <satharva2004@gmail.com>
2026-03-24 11:21:57 +0530

    Allow Vercel frontend in backend CORS

--- server/app/server.py
+++ server/app/server.py
@@ -16,7 +16,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://basti-ka-hasti-ml-t9kk.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
